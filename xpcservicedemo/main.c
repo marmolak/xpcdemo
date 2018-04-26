079a49bb36ef544ca2dd587bcdafd52cfacc4fc4
Robin Hack <hack.robin@gmail.com>
2018-04-26 14:52:56 +0200

    Initial Commit

---
+++ xpcservicedemo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  xpcservicedemo
+//
+//  Created by Robin Hack on 26/04/2018.
+//  Copyright © 2018 Robin Hack. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
